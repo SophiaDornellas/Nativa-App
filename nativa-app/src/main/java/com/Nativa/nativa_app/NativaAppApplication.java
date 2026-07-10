ff018f144fbb76c1588921238cebd654e38f0006
SophiaDornellas <sophiadornellas7@gmail.com>
2026-07-10 11:29:57 -0300

    feat: inicialização do projeto Java com Spring Boot

---
+++ nativa-app/src/main/java/com/Nativa/nativa_app/NativaAppApplication.java
@@ -0,0 +1,13 @@
+package com.Nativa.nativa_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativaAppApplication.class, args);
+	}
+
+}
---
+++ nativa-app/src/test/java/com/Nativa/nativa_app/NativaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nativa.nativa_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
